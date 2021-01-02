83281405930904f760a541826eac040b75689240
epicnl <rene@epicnl.com>
2021-01-02 23:31:12 +0100

    Changed text

--- CiConsoleTest/Program.cs
+++ CiConsoleTest/Program.cs
@@ -6,7 +6,7 @@ namespace CiConsoleTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is awesome!");
         }
     }
 }
